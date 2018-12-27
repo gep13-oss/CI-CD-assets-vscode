aea2dd594eb4c1c05d0d343cc797578749d46dd8
Gary Ewan Park <gep13@gep13.co.uk>
2018-12-27 09:09:16 +0000

    (GH-5) Add ability to download .mergify.yml file

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,7 @@ export function activate(): void {
     commands.registerCommand("cicd.appveyor", async () => downloadAppVeyorConfigFile());
     commands.registerCommand("cicd.gitattributes", async () => downloadGitAttributesFile());
     commands.registerCommand("cicd.gitignore", async () => downloadGitIgnoreFile());
+    commands.registerCommand("cicd.mergify", async () => downloadMergifyFile());
 }
 
 async function checkForExisting(path: string): Promise<boolean> {
@@ -164,6 +165,35 @@ async function downloadGitIgnoreFile(): Promise<void> {
       window.showInformationMessage(".gitignore File downloaded correctly.");
     } else {
       window.showErrorMessage("Error downloading .gitignore File.");
+    }
+  }
+}
+
+async function downloadMergifyFile(): Promise<void> {
+  var workspaceRootPath = checkForWorkspace();
+  if(workspaceRootPath !== "") {
+    var mergifyFilePath = path.join(workspaceRootPath, '.mergify.yml');
+    var ready = await checkForExisting(mergifyFilePath);
+
+    if(!ready) {
+      return;
+    }
+
+    var file = fs.createWriteStream(mergifyFilePath);
+    var config = workspace.getConfiguration('cicd');
+
+    if (!config) {
+      window.showErrorMessage("Could not find CI/CD Configuration.");
+      return;
+    }
+
+    var uri = config.urls.mergify;
+    var result = await downloadFile(uri, file);
+
+    if(result) {
+      window.showInformationMessage(".mergify.yml File downloaded correctly.");
+    } else {
+      window.showErrorMessage("Error downloading .mergify.yml File.");
     }
   }
 }
