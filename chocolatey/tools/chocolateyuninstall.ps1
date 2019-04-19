53d74a8268cb43fbdd4f29d5b07b859cb9a30f50
Gary Ewan Park <gep13@gep13.co.uk>
2019-04-19 21:58:47 +0100

    (GH-42) Switch to using Chocolatey Extension

--- chocolatey/tools/chocolateyuninstall.ps1
+++ chocolatey/tools/chocolateyuninstall.ps1
@@ -1,11 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-. "$toolsDir\Uninstall-VsCodeExtension.ps1"
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  extensionName = 'gep13.ci-cd-assets-vscode'
-}
-
-Uninstall-VsCodeExtension @packageArgs
+Uninstall-VsCodeExtension "gep13.ci-cd-assets-vscode-vscode"
